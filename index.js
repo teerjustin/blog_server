4006820ddf81a453ed12763a7a31c5b2da4abab7
teerjustin <teerjustin@gmail.com>
2022-11-23 17:24:37 -0800

    changed port

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use('/posts', postRoutes)
 //mongodb / atltas
 
 const CONNECTION_URL = 'mongodb+srv://JustinTeer:JustinTeer123@cluster0.jd0xczb.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
